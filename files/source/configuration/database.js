edb4a94ad0479179ff9f70c629ed001f2c5d9160
Aykut Aydınlı <aykut@pyronome.com>
2020-03-12 17:55:01 +0300

    Files updated

--- files/source/configuration/database.js
+++ files/source/configuration/database.js
@@ -1,9 +1,9 @@
 var Database = {
-    "database_name": "",
-    "user": "",
-    "password": "",
-    "host": "",
-    "type": ""
+    "database_name": "", /* {{snippet:database_name}} */
+    "user": "", /* {{snippet:user}} */
+    "password": "", /* {{snippet:password}} */
+    "host": "", /* {{snippet:host}} */
+    "type": "" /* {{snippet:type}} */
 };
 
 module.exports = Database;
